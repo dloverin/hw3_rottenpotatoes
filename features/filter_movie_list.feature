4d6683a41696864531df2110dbe2fda9a2f4ac65
dloverin <darrell.loverin@gmail.com>
2013-08-07 17:41:22 -0700

    part2

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,24 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: PG, R
+
   # enter step(s) to uncheck all other checkboxes
+  Given I uncheck the following ratings: G, PG-13, NC-17
+  
   # enter step to "submit" the search form on the homepage
+  When I press "ratings_submit"
+
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see the following movies: PG, R
+
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see the following movies: G, PG-13, NC-17
 
 Scenario: no ratings selected
-  # see assignment
+  Given I uncheck the following ratings: G, PG, PG-13, NC-17, R
+  Then I should see all of the movies
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: G, PG, PG-13, NC-17, R
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -26,4 +26,39 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(%r{,\s*}) || []
+  ratings.each do |rating|
+    step "I #{(uncheck ? "uncheck" : "check")} \"ratings_#{rating}\""
+  end
+end
+
+Then /I should (not )?see the following movies: (.*)/ do |hidden, rating_list|
+  ratings = rating_list.split(%r{,\s*}) || []
+  results = all("tbody tr")
+  filtered_count = 0
+  ratings.each do |rating|
+    results.each do |result|
+      if (result.text.index("\n#{rating}\n")) 
+        filtered_count += 1
+      end
+    end
+  end
+
+  if (hidden)
+    filtered_count.should == 0
+  else
+    movies = Movie.find_all_by_rating(ratings)
+    filtered_count.should == movies.length
+  end
+end
+
+Then /I should see all of the movies/ do
+  all_movies = Movie.all
+  results = all("tbody tr")
+  all_movies.length.should == results.length
+end
+
+Then /I should see no movies/ do
+  results = all("tbody tr")
+  results.length.should == 0
 end
